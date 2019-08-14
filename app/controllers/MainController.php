4efb1be84c5f988719e35e897fa894be7237d619
Alexsander <alexsander11115@gmail.com>
2019-08-14 17:21:31 +0300

    Add cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,6 +6,8 @@ namespace app\controllers;
 
 //use RedBeanPHP\R;
 
+use ishop\Cache;
+
 class MainController extends AppController
 {
 
@@ -13,6 +15,13 @@ class MainController extends AppController
         $posts = \R::findAll('test');
 
         $this->setMeta(['title'=> 'Main','description'=>'MainDescription']);
+        $cache = Cache::instanse();
+//        $cache->set('test',$posts);
+
+        $data = $cache->get('test');
+        if(!$data){
+            $cache->set('test',$posts);
+        }
         $name = "Alex";
         $age = 28;
         $this->set(compact('name','age','posts'));
--- vendor/ishop/core/Db.php
+++ vendor/ishop/core/Db.php
@@ -14,12 +14,13 @@ use TSingletone;
         $db = require_once CONF. '/config_db.php';
         class_alias('\RedBeanPHP\R','\R');
         \R::setup($db['dsn'],$db['user'],$db['pass']);
+        if(!\R::testConnection()){
+            throw new \Exception('Нет соеденения с бд',500);
+        }
         \R::freeze(true);
         if (DEBUG){
             \R::debug(true,1);
         }
-        if(!\R::testConnection()){
-            throw new \Exception('Нет соеденения с бд',500);
-        }
+
     }
 }
